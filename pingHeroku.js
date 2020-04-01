d4095915988f44e4c5844e6bd56e89987575d48e
Henry <hnryzhng@gmail.com>
2020-04-01 09:52:57 +0800

    added pingHeroku file

--- pingHeroku.js
+++ pingHeroku.js
@@ -1,13 +1,13 @@
 // Heroku App Pinger
 
-// prevent Heroku app from sleeping in free dynos mode
+// prevent Heroku app from sleeping after 30 minutes for free dynos
 
 var http = require('http');
 
 function pingHeroku(appUrl, pingInterval) {
 	setInterval(function() {
-		http.get(appUrl);	
-	}, pingInterval);	// interval between pings in milliseconds
+		http.get(appUrl);
+	}, pingInterval);	// interval in milliseconds
 };
 
 module.exports = pingHeroku;
